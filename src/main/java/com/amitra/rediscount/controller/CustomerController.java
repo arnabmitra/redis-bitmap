bca6cdfb32b839dac5fcbd28d1e6a13561ebf030
Arnab Mitra <arnabmitra.mitra@gmail.com>
2017-05-28 23:34:04 -0600

    More changes,

--- src/main/java/com/amitra/rediscount/controller/CustomerController.java
+++ src/main/java/com/amitra/rediscount/controller/CustomerController.java
@@ -1,18 +1,27 @@
 package com.amitra.rediscount.controller;
 
+import com.amitra.rediscount.model.ActionEvent;
+import com.amitra.rediscount.service.CustomerService;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * Created by amitra on 5/28/17.
- */
+import javax.inject.Inject;
+import java.util.concurrent.ExecutionException;
+
+
 @RestController
 @RequestMapping(value="/customer")
 public class CustomerController {
 
+  @Inject CustomerService customerService;
 
-//    @RequestMapping("/hello/{name}")
-//    String hello(@PathVariable String name) {
-//      return "Hello, " + name + "!";
-//  }
+  @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+  @ResponseBody
+  public void post( @RequestBody ActionEvent actionEvent) throws ExecutionException, InterruptedException {
+    customerService.setUserAction("userAction:"+actionEvent.getEventType(),actionEvent.getCustomerId());
+  }
 }
--- src/main/java/com/amitra/rediscount/service/CustomerService.java
+++ src/main/java/com/amitra/rediscount/service/CustomerService.java
@@ -3,11 +3,23 @@ package com.amitra.rediscount.service;
 
 public interface CustomerService {
 
+  /**
+   * Set the user action for the customer.
+   * @param userKey The key to the bitmap.
+   * @param customerId the customer id for which this is being looked up for.
+   */
   void setUserAction(String userKey, long customerId);
 
+  /**
+   * Has an user action been performed.
+   * @param userKey The key to the bitmap.
+   * @param customerId the customer id for which this is being looked up for.
+   * @return Boolean true or false.
+   */
   boolean isUserAction(String userKey,Long customerId);
 
   long getNumberOfUserClicksPerAction(String userKey);
 
   long getNumberOfUserClicksAllAction(String... userKey);
+
 }
